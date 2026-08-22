28b1e530c3a193de5f077de3a0a3efcde4058bef
Justin Breulmann <jb824851@fh-muenster.de>
2026-08-22 17:32:25 +0200

    chore: remove typedoc plugins config and script

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,6 +1,6 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
-import { getTypedocPlugins } from './typedoc-plugins';
+
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
@@ -70,9 +70,7 @@ const config: Config = {
     ],
   ],
 
-  plugins: [
-    ...getTypedocPlugins()
-  ],
+  plugins: [],
 
   themeConfig: {
     // Replace with your project's social card
