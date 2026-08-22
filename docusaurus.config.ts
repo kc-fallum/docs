d530436f3a35a52c26cdd001a47c0e0b0c8b0cd8
Justin Breulmann <jb824851@fh-muenster.de>
2026-08-22 17:14:15 +0200

    fix: resolve broken links by setting routeBasePath to root

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -42,6 +42,7 @@ const config: Config = {
       {
         docs: {
           sidebarPath: './sidebars.ts',
+          routeBasePath: '/',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
@@ -113,7 +114,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/intro',
             },
           ],
         },
